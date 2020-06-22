53045d7fabd5aa6257a8a0824d4654aed015be60
Stephen Fang <safang99@gmail.com>
2020-06-22 12:05:31 -0400

    final code

--- src/components/Playlist.js
+++ src/components/Playlist.js
@@ -3,7 +3,40 @@ import React from 'react';
 import Track from './Track.js';
 
 const Playlist = props => {
+  let songs = props.songs
+  // debugger
 
+  const songComponents = songs.map((song) => {
+    return <Track />
+  })
+let trackOne = 
+  <Track 
+    title={songs[0].title}
+    duration={songs[0].duration}
+    playing={songs[0].playing}
+  />;
+
+let trackTwo = 
+  <Track 
+    title={songs[1].title}
+    duration={songs[1].duration}
+    playing={songs[1].playing}
+  />;
+
+let trackThree = 
+  <Track 
+    title={songs[2].title}
+    duration={songs[2].duration}
+    playing={songs[2].playing}
+  />;
+
+  return(
+  <ul className="tracks">
+   {trackOne}
+   {trackTwo}
+   {trackThree}
+  </ul>
+  )
 }
 
 export default Playlist;
--- src/components/Track.js
+++ src/components/Track.js
@@ -1,7 +1,24 @@
 import React from 'react';
 
 const Track = props => {
+  let title = props.title
+  let duration = props.duration
+  let playing = props.playing
 
+  let playName
+  if (playing) {
+    playName = "fas fa-pause-circle"
+  } else {
+    playName = "fas fa-play-circle"
+  }
+  // debugger
+
+  return(
+    <li>
+      <i className={playName}></i>
+      {title} - {duration}
+    </li>
+  )
 }
 
 export default Track;
