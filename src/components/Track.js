f6fabce10f34e0027cee055d312e8042d17d59db
nwalberts <nwalberts5@gmail.com>
2020-12-21 13:20:36 -0500

    final

--- src/components/Track.js
+++ src/components/Track.js
@@ -1,7 +1,19 @@
-import React from 'react';
+import React from "react"
 
-const Track = props => {
+const Track = (props) => { 
+  let songSymbol
+  if (props.playing === true) {
+    songSymbol = "fa-pause-circle"
+  } else {
+    songSymbol = "fa-play-circle"
+  }
 
+  return(
+    <li className="track">
+      <i className={`far ${songSymbol}`}></i>
+      {props.artist} - {props.title} - {props.duration}
+    </li>
+  )
 }
 
-export default Track;
+export default Track
--- src/main.js
+++ src/main.js
@@ -1,32 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-// let data = [
-//   { id: 1, title: "Talking Heads - Found A Job", playing: false, duration: "4:30" },
-//   { id: 2, title: "Phoenix - Entertainment", playing: false, duration: "3:40" },
-//   { id: 3, title: "Chrome Sparks - Moonraker", playing: true, duration: "6:04" }
-// ];
+import Playlist from "./components/Playlist"
 
-let trackOne = <li className="track">
-  <i className="fa fa-play-circle-o" />
-  Talking Heads - Found A Job - 4:30
-</li>;
-
-let trackTwo = <li className="track">
-  <i className="fa fa-play-circle-o" />
-  Phoenix - Entertainment - 3:40
-</li>;
-
-let trackThree = <li className="track selected">
-  <i className="fa fa-pause-circle-o" />
-  Chrome Sparks - Moonraker - 6:04
-</li>;
+let trackData = [
+  { id: 1, title: "Talking Heads - Found A Job", playing: false, duration: "4:30" },
+  { id: 2, title: "Phoenix - Entertainment", playing: false, duration: "3:40" },
+  { id: 3, title: "Chrome Sparks - Moonraker", playing: true, duration: "6:04" }
+];
 
 ReactDOM.render(
-  <ul className="tracks">
-    {trackOne}
-    {trackTwo}
-    {trackThree}
-  </ul>,
+    <Playlist 
+      trackData={trackData}
+    />,
   document.getElementById('app')
 );
