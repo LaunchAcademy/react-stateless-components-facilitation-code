096589f3cf1c0aadb338942068dc22d7dd493fce
dovakeen118 <kegillis118@gmail.com>
2021-06-21 13:16:10 -0400

    finished code

--- src/components/Playlist.js
+++ src/components/Playlist.js
@@ -3,7 +3,33 @@ import React from 'react';
 import Track from './Track.js';
 
 const Playlist = props => {
+  const trackCollection = props.songData.map((song) => {
+    // debugger
 
+    // let songIcon = "fas fa-play-circle"
+    // if (song.playing === true) {
+    //   songIcon = "fas fa-pause-circle"
+    // }
+
+    // songIcon={songIcon}
+    return (
+      <Track 
+        key={song.id}
+        title={song.title}
+        playing={song.playing}
+        duration={song.duration}
+      />
+    )
+  })
+  // debugger
+  // console.log(trackCollection)
+  // [<Track track={data[0]} />, <Track track={data[1]} />, <Track />]
+  return (
+    <div>
+      <h1>My Playlists</h1>
+      {trackCollection}
+    </div>
+  )
 }
 
 export default Playlist;
--- src/components/Track.js
+++ src/components/Track.js
@@ -1,7 +1,20 @@
 import React from 'react';
 
 const Track = props => {
+  // const songPlaying = "fas fa-pause-circle"
+  // const icon = "fas fa-play-circle"
+  
+  let songIcon = "fas fa-play-circle"
+  if (props.playing === true) {
+    songIcon = "fas fa-pause-circle"
+  }
 
+  return (
+    <li className="track">
+      <i className={songIcon} />
+      {props.title} - {props.duration}
+    </li>
+  )
 }
 
 export default Track;
