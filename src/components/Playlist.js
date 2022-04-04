a09973e0cf46cbd3b29322ca791b573a4994cffb
dovakeen118 <kegillis118@gmail.com>
2022-04-04 12:02:29 -0400

    finished fac code

--- src/components/Playlist.js
+++ src/components/Playlist.js
@@ -1,9 +1,34 @@
-// src/components/Playlist.js
-import React from 'react';
-import Track from './Track.js';
+import React from "react"
 
-const Playlist = props => {
+import Track from "./Track"
 
+const Playlist = (props) => {
+  // any logic
+  // debugger
+  
+  const songs = props.songsData.map((song) => {
+    return (
+      <Track song={song} />
+    )
+  })
+
+  // const songs = props.songsData.map((song) => {
+  //   return (
+  //     <li className="track">
+  //       <i className="fas fa-play-circle"></i>
+  //       {song.title} - {song.duration}
+  //     </li>
+  //   )
+  // })
+
+  return (
+    <>
+      <h1>Playlist</h1>
+      <ul>
+        {songs}
+      </ul>
+    </>
+  )
 }
 
-export default Playlist;
+export default Playlist
--- src/components/Track.js
+++ src/components/Track.js
@@ -1,7 +1,22 @@
 import React from 'react';
 
-const Track = props => {
+const Track = (props) => {
+  // debugger
 
+  // fa-pause-circle
+  // fa-play-circle
+
+  let playingIcon = "fa-pause-circle"
+  if (props.song.playing) {
+    playingIcon = "fa-play-circle"
+  }
+
+  return ( 
+    <li className="track">
+      <i className={`fas ${playingIcon}`}></i>
+      {props.song.title} - {props.song.duration}
+    </li>
+  )
 }
 
 export default Track;
